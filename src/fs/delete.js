24d6f5866e51d43127fe6dcfe79088c342a7bfd3
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 15:02:27 +0300

    feat: implement fs/delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import { access, constants, rm } from "fs";
+
 const remove = async () => {
-    // Write your code here 
+	const filePath = `${import.meta.dirname}/files/fileToRemove.txt`
+	access(filePath, constants.F_OK, (err) => {
+		if (err) throw new Error("FS operation failed");
+		rm(filePath, (err) => {
+			if (err) console.log(err);
+		})
+	})
 };
 
 await remove();
