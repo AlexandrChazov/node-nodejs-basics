774f005de1fadd46b92490f913ed20d335ff3168
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 16:36:27 +0300

    feat: implement cli/env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    let result = "";
+    Object.keys(process.env).forEach((name) => {
+        if (name.slice(0,4) === "RSS_") {
+            result = `${result}${name}=${process.env[name]}; `
+        }
+    })
+    if (result) console.log(result);
 };
 
 parseEnv();
