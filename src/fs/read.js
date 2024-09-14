0241515e2af6cae6f551117c36d55bf73f43b219
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 15:33:15 +0300

    feat: implement fs/read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import { readFile } from "fs";
 const read = async () => {
-    // Write your code here 
+	 readFile(`${import.meta.dirname}/files/fileToRead.txt`, "utf-8", (err, data) => {
+		 if (err) {
+			 throw new Error("FS operation failed")
+		 } else {
+			 console.log(data);
+		 }
+	 })
 };
 
 await read();
