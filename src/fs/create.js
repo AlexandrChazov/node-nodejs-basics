cd24321425697db5fb98b20f4750699fad84a499
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 09:59:21 +0300

    feat: implement fs/create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { writeFile, access, constants } from "fs";
 const create = async () => {
-    // Write your code here 
+    const filePath = `${import.meta.dirname}/files/fresh.txt`;
+
+    await access(filePath, constants.R_OK, (err) => {
+        // file exist
+        if (!err) throw new Error("FS operation failed");
+    });
+
+    await writeFile(filePath,"I am fresh and young", "utf-8", (err) => {
+        if (err) console.log(err);
+    });
 };
 
 await create();
