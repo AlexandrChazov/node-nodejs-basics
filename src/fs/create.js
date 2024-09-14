16b1a8adefbb50893640de4c748604dfda08003d
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 11:36:06 +0300

    fix: stop executing of function on acess error

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,15 +1,14 @@
-import { writeFile, access, constants } from "fs";
+import {writeFile, access, constants} from "fs";
 const create = async () => {
     const filePath = `${import.meta.dirname}/files/fresh.txt`;
-
-    await access(filePath, constants.R_OK, (err) => {
+    access(filePath, constants.F_OK, (err) => {
         // file exist
         if (!err) throw new Error("FS operation failed");
+        writeFile(filePath,"I am fresh and young", "utf-8", (err) => {
+            if (err) console.log(err);
+        });
     });
 
-    await writeFile(filePath,"I am fresh and young", "utf-8", (err) => {
-        if (err) console.log(err);
-    });
 };
 
 await create();
