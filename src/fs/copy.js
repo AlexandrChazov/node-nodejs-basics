322a4aa4da5851d1a7cd397aa469391e3e0827db
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 10:27:43 +0300

    feat: implement fs/copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
+import { access, constants, cp  } from "fs";
 const copy = async () => {
-    // Write your code here 
+		const sourcePath = `${import.meta.dirname}/files`;
+		const destPath = `${import.meta.dirname}/files_copy`;
+    await access(destPath, constants.O_DIRECTORY, (err) => {
+			if (!err) throw new Error("FS operation failed");
+		})
+		await cp(sourcePath, destPath, { recursive: true }, () => {})
 };
 
 await copy();
