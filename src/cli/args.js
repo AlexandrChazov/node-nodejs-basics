b622597277c45a920af10b1ea617dd078c34e933
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 16:53:56 +0300

    feat: implement cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+		let result = "";
+		const args = process.argv;
+		for (let i = 0; i < args.length; i++) {
+			if (args[i].slice(0,2) === "--") {
+				result = `${result}${args[i].slice(2)} is ${args[i+1]}; `
+			}
+		}
+		if (result) console.log(result);
 };
 
 parseArgs();
