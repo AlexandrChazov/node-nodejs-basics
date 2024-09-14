4e589d1636c9b8c26e957c4bbb3ac96cc0a2f3fd
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 11:38:23 +0300

    fix: stop function executing on access error

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,11 +1,11 @@
 import { access, constants, cp  } from "fs";
 const copy = async () => {
-		const sourcePath = `${import.meta.dirname}/files`;
-		const destPath = `${import.meta.dirname}/files_copy`;
-    await access(destPath, constants.O_DIRECTORY, (err) => {
-			if (!err) throw new Error("FS operation failed");
-		})
-		await cp(sourcePath, destPath, { recursive: true }, () => {})
+	const sourcePath = `${import.meta.dirname}/files`;
+	const destPath = `${import.meta.dirname}/files_copy`;
+	access(destPath, constants.O_DIRECTORY, (err) => {
+		if (!err) throw new Error("FS operation failed");
+		cp(sourcePath, destPath, { recursive: true }, () => {})
+	})
 };
 
 await copy();
