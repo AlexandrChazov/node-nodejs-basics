da4616df8a29d02238804d6b0790d7aee0c18a48
Alexandr Chazov <chazovby@gmail.com>
2024-09-14 15:25:35 +0300

    feat: implement fs/list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
+import {access, constants, readdir} from "fs";
 const list = async () => {
-    // Write your code here 
+    const dirPath = `${import.meta.dirname}/files`;
+		access(dirPath, constants.F_OK, (err) => {
+			if (err) throw new Error("FS operation failed");
+			readdir(dirPath, (err, files) => {
+				err ?	console.log(err) : console.log(files)
+			})
+		})
 };
 
 await list();
