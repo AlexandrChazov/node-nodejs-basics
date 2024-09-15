e97663d9a824226f8cb8dd5dc7c5a20a42ac231e
Alexandr Chazov <chazovby@gmail.com>
2024-09-15 08:22:22 +0300

    feat: implement 'streams/read' task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from "fs";
+
 const read = async () => {
-    // Write your code here 
+	const filePath = `${import.meta.dirname}/files/fileToRead.txt`;
+  const readStream = createReadStream(filePath);
+
+	readStream.on('data', (chunk) => {
+		process.stdout.write(chunk);
+	})
+	readStream.on('error', () => {
+		console.log("Something went wrong");
+	})
 };
 
 await read();
