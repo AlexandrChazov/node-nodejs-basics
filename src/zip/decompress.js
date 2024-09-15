be6d56d24bbe1cc452a50ae676c9202d145fa1bf
Alexandr Chazov <chazovby@gmail.com>
2024-09-15 13:24:23 +0300

    feat: implement 'zip/decompress' task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createUnzip } from "zlib";
 const decompress = async () => {
-    // Write your code here 
+	const sourcePath = `${import.meta.dirname}/files/archive.gz`;
+	const destPath = `${import.meta.dirname}/files/fileToCompress.txt`;
+	const readStream = createReadStream(sourcePath);
+	const writeStream = createWriteStream(destPath);
+	const unzip = createUnzip();
+
+	function handleError() {
+		console.log("Decompressed with error");
+		readStream.destroy();
+		writeStream.end("Finished with error...")
+	}
+
+	readStream
+		.on("error", handleError)
+		.pipe(unzip)
+		.pipe(writeStream)
+		.on("error", handleError);
 };
 
 await decompress();
