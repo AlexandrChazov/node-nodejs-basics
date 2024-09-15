d4eccad7e214926831be311fcfd3ec1efaa9a108
Alexandr Chazov <chazovby@gmail.com>
2024-09-15 07:45:14 +0300

    feat: implement 'hash' task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
 const calculateHash = async () => {
-    // Write your code here 
+	const filePath = `${import.meta.dirname}/files/fileToCalculateHashFor.txt`
+  const readStream = createReadStream(filePath);
+	const hash = createHash("sha256");
+
+	readStream.on("data", (buffer) => {
+		hash.update(buffer);
+	})
+	readStream.on("error", () => {
+		console.log("Something went wrong");
+	})
+	readStream.on("end", () => {
+		console.log(hash.digest("hex"));
+	})
 };
 
 await calculateHash();
