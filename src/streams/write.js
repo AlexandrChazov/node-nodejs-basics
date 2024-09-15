90b7739faeaac7cb866e9656165bb95b5bdc9e4c
Alexandr Chazov <chazovby@gmail.com>
2024-09-15 08:53:36 +0300

    feat: implement 'streams/write' task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from "fs";
 const write = async () => {
-    // Write your code here 
+	const filePath = `${import.meta.dirname}/files/fileToWrite.txt`;
+
+	const writeStream = createWriteStream(filePath);
+  process.stdin.on("data", (input) => {
+		writeStream.write(input);
+	})
+	process.stdin.on("end", () => {
+		writeStream.end();
+	})
+
+	process.stdin.on("error", () => {
+		console.log("Something went wrong")
+	})
+	writeStream.on("error", () => {
+		console.log("Something went wrong")
+	})
 };
 
 await write();
