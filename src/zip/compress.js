54af4cc65b0a497644979ec8f13c0d5a3389f4b5
Alexandr Chazov <chazovby@gmail.com>
2024-09-15 13:24:11 +0300

    feat: implement 'zip/compress' task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
 const compress = async () => {
-    // Write your code here 
+    const sourcePath = `${import.meta.dirname}/files/fileToCompress.txt`;
+    const distPath = `${import.meta.dirname}/files/archive.gz`;
+		const gzip = createGzip();
+		const readStream = createReadStream(sourcePath);
+		const writeStream = createWriteStream(distPath);
+
+		function handleError(err) {
+			console.log("Finished with error...");
+			readStream.destroy();
+			writeStream.end("Finished with error...");
+		}
+
+		readStream
+			.on("error", handleError)
+			.pipe(gzip)
+			.pipe(writeStream)
+			.on("error", handleError);
 };
 
 await compress();
