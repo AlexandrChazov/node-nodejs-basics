11c83199da4e348f66b689770e4444510a207a8f
Alexandr Chazov <chazovby@gmail.com>
2024-09-16 10:32:33 +0300

    feat: implement 'Child processes' task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from "child_process";
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	const child = spawn('node', ['src/cp/files/script.js', ...args], {
+		stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+	});
+	// Forward data from master process stdin to child process stdin
+	process.stdin.pipe(child.stdin);
+
+	// Forward data from child process stdout to master process stdout
+	child.stdout.pipe(process.stdout);
+
+	child.on('exit', (code) => {
+		console.log(`Child process exited with exit code ${code}`);
+	});
+	child.on('error', (err) => {
+		console.log("Something went wrong", err);
+	});
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['hello', 'world']);
