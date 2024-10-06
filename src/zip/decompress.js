a9cf288f1d4aea6c953beab2f1e1200c435b6813
Alexandr Chazov <chazovby@gmail.com>
2024-10-06 08:47:30 +0300

    fix: delete sourse file after decompression

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,4 +1,4 @@
-import { createReadStream, createWriteStream } from "fs";
+import { createReadStream, createWriteStream, rm } from "fs";
 import { createUnzip } from "zlib";
 const decompress = async () => {
 	const sourcePath = `${import.meta.dirname}/files/archive.gz`;
@@ -7,17 +7,28 @@ const decompress = async () => {
 	const writeStream = createWriteStream(destPath);
 	const unzip = createUnzip();
 
-	function handleError() {
+	readStream
+		.on("error", handleError(readStream, writeStream))
+		.pipe(unzip)
+		.pipe(writeStream)
+		.on("finish", deleteFile(sourcePath))
+		.on("error", handleError(readStream, writeStream));
+};
+
+await decompress();
+
+function handleError(readStream, writeStream) {
+	return function () {
 		console.log("Decompressed with error");
 		readStream.destroy();
 		writeStream.end("Finished with error...")
 	}
+}
 
-	readStream
-		.on("error", handleError)
-		.pipe(unzip)
-		.pipe(writeStream)
-		.on("error", handleError);
-};
-
-await decompress();
+function deleteFile(path) {
+	return function () {
+		rm(path, (err) => {
+			if (err) console.error("Failed to delete source file");
+		})
+	}
+}
