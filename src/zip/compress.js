4a07804e72aa6d0377387ce939bda49557e30d89
Alexandr Chazov <chazovby@gmail.com>
2024-10-06 08:40:27 +0300

    fix: delete sourse file after compression

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,4 +1,4 @@
-import { createReadStream, createWriteStream } from "fs";
+import { createReadStream, createWriteStream, rm } from "fs";
 import { createGzip } from "zlib";
 
 const compress = async () => {
@@ -8,17 +8,28 @@ const compress = async () => {
 		const readStream = createReadStream(sourcePath);
 		const writeStream = createWriteStream(distPath);
 
-		function handleError(err) {
-			console.error("Finished with error...");
-			readStream.destroy();
-			writeStream.end("Finished with error...");
-		}
-
 		readStream
-			.on("error", handleError)
+			.on("error", handleError(readStream, writeStream))
 			.pipe(gzip)
 			.pipe(writeStream)
-			.on("error", handleError);
+			.on("finish", deleteFile(sourcePath))
+			.on("error", handleError(readStream, writeStream));
 };
 
 await compress();
+
+function handleError(readStream, writeStream) {
+	return function () {
+		console.error("Finished with error...");
+		readStream.destroy();
+		writeStream.end("Finished with error...");
+	}
+}
+
+function deleteFile(path) {
+	return function () {
+		rm(path, (err) => {
+			if (err) console.error("Failed to delete source file");
+		})
+	}
+}
